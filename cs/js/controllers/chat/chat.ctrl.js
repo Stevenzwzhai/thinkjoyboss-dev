8a6975d4d63c291dee714be0dc5bb86879bd6d92
abjia <daohao1914>
2015-01-27 20:29:04 +0800

    测试

--- cs/js/controllers/chat/chat.ctrl.js
+++ cs/js/controllers/chat/chat.ctrl.js
@@ -2,53 +2,51 @@ App
 
     .controller("ChatCtrl", function ($rootScope,$scope, $window, $log, $q, $timeout, AuditService,Util,ChatSev) {
 
-            console.log("chat...");
+              //用户列表
+             $scope.users = [];
+
 
             $scope.userName = "admin";
+            $scope.userIcon = "lib/img/a0.jpg";
 
             //切换显示
             $scope.isToggle = true;
 
-            //发送消息
-            $scope.sendVal = "";
-
             //聊天列表
-            $scope.posts = [
-                {
-                    msgBody : "你好的我是小明...",
-                    msgTime  : "10:12",
-                    username  : "小明",
-                    msgImg    : "lib/img/a0.jpg",
-                    msgFrom   : "1"
-                },
-                {
-                    msgBody : "你好的我是小明...",
-                    msgTime  : "10:12",
-                    username  : "小明",
-                    msgImg    : "lib/img/a0.jpg",
-                    msgFrom   : "2"
-                },
-                {
-                    msgBody : "你好的我是小明...",
-                    msgTime  : "10:12",
-                    username  : "小明",
-                    msgImg    : "lib/img/a0.jpg",
-                    msgFrom   : "3"
-                }
-            ];
+            $scope.posts = [];
 
-            //用户列表
-            $scope.userList = [];
+            //当前激活的用户
+            $scope.currentUser = {};
+
+            //当前内容
+            $scope.fm = {
+                content : ""
+            }
+
+
+            function Message (body,icon,userId,userName){
+                    this.body = body;
+                    this.isReceive = false;
+                    this.sendTime = new Date().getTime();
+                    this.userIcon =  icon;
+                    this.userId = userId;
+                    this.userName = userName
+
+            }
+
+
+
 
             //切换聊天
-            $scope.toggleChat = function(toggle){
+            $scope.toggleChat = function(toggle,user){
 
                 if(toggle){
                     $scope.isToggle = true;
-
+                    $scope.currentUser = "";
                 }
                 else{
                     $scope.isToggle = false;
+                    $scope.currentUser = user || "";
                 }
 
             }
@@ -57,31 +55,67 @@ App
             $scope.enter = function(ev){
                 if (ev.keyCode !== 13) return;
                 console.log("发送...");
-                $scope.sendVal = "";
-                //清空
-            }
-
-            //接受消息
-            var  onMessage = function(event){
-                var data = JSON.parse(event.data);
-                switch (data.type){
-                    case "broadcast" :  //获取广播到的新用户
-                        $scope.userList.unshift(data.userInfo);
-                        console.log($scope.userList);
-                        break;
-                    case "userList" :   //获取历史聊天用户列表
-
-                        break;
-                    default :
-                        break;
+                var currentUser =   $scope.currentUser;
+                var contnet = $scope.fm.content;
+                if(contnet == ""){
+                    $rootScope.alertInfo("写点什么啊！");
+                    return;
                 }
 
 
+                var message =  angular.copy(new Message(contnet,$scope.userIcon,"01000",$scope.userName));
+
+                if(!$scope.posts){
+                    $scope.posts = [];
+                }
+
+                $scope.posts.push(message);
+
+                var u = searchUser(currentUser.userId);
+
+
+                if(!u.posts)
+                    u.posts = [];
+
+                u.posts.push(message);
+                $rootScope.alertSuccess("消息已经发送!");
+                ChatSev.sendMessage($scope.userName,currentUser.userId,contnet);
+
+                //清空
+                $scope.fm.content = "";
+
             }
+
+
+        //显示聊天窗口
+        $scope.showMessage = function(user){
+
+            //新用户
+            if(user.isNew){
+
+                var isCon = window.confirm("用户 " + user.userName+" 咨询请求,是否接受？");
+
+                if(isCon){
+                    ChatSev.addUser($scope.userName,user.userId,user.key);
+                }
+                else{
+                    removeUser(user.userId);
+                }
+
+            }
+            else{
+                $scope.posts =  user.posts;
+                $scope.toggleChat(false,user);
+//                $scope.$apply();
+            }
+
+        }
+
 
 
           //建立连接
           ChatSev.open($scope.userName).then(function(res){
+
               ChatSev.getUserList($scope.userName);
               ChatSev.socket.onmessage = onMessage;
           }).then(function(err){
@@ -91,5 +125,82 @@ App
 
 
 
+        //接受消息
+        var  onMessage = function(event){
+            var data = JSON.parse(event.data);
+                switch (data.type){
+                    case "broadcast" :  //获取广播到的新用户
+                        data.userInfo.key = data.key;
+                        $scope.users.push(data.userInfo);
+                        break;
+                    case "userList" :   //获取历史聊天用户列表
+                        $scope.users =  data.userList;
+                        break;
+                    case "acceptResult" :   //验证结果
+                        if(data.result){
+                            $rootScope.alertSuccess("已接受此用户咨询！");
+                        }
+                        else{
+                            $rootScope.alertError("已经有客服接受次用户!");
+                            removeUser(data.to);
+                        }
+
+                        break;
+                    case "receive" :   //接受消息
+                        data.msg.isReceive = true;
+                        var user = searchUser(data.msg.userId);
+                        if(!user.posts)
+                            user.posts = [];
+
+                        //用户列表添加message
+                        user.posts.push(data.msg);
+
+                        console.log(data.msg);
+
+                        if(!$scope.posts){
+                            $scope.posts = [];
+                        }
+
+                        if(user.userId == $scope.currentUser.userId){
+                            $scope.posts.push(data.msg);
+                        }
+
+                        break;
+
+                    default :
+                        break;
+
+                }
+
+            console.log(data.type);
+                $scope.$apply();
+        }
+
+
+
+        //删除列表用户
+        var removeUser = function(userId){
+            $scope.users.forEach(function(user,index){
+                if(user.userId == userId)
+                    $scope.users.splice(index,1);
+            });
+        }
+
+
+        //查找到用户列表
+        var  searchUser = function(userId){
+            for(var i=0;i<$scope.users.length;i++){
+                var u = $scope.users[i];
+                if(u.userId == userId){
+                    return  u;
+                }
+            }
+        }
+
+
+
 
     });
+
+
+
