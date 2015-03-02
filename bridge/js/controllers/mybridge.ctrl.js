62bc944022060dfd1d9436097bfec18ffa955afb
abjia <daohao1914>
2015-03-02 11:58:30 +0800

    boss链接线上

--- bridge/js/controllers/mybridge.ctrl.js
+++ bridge/js/controllers/mybridge.ctrl.js
@@ -1,5 +1,26 @@
-App.controller("MyBridgeCtrl",function($rootScope){
+App.controller("MyBridgeCtrl",function($rootScope,$scope,Util){
     $rootScope.currentSys = "mBridge";
 //    console.log("mybridge ctrl...");
+
+    //从local中去判断
+    var newRight = Util.getSgObj("bridgeRight");
+
+    if(!newRight){
+
+        //权限来控制显隐
+        var  rightList = $rootScope.user.roleList.filter(function(role){
+            if(role.sysCode == "mBridge") return true;
+        });
+
+        //拿到url集合
+        newRight = rightList[0].resourceList.map(function(right){
+            return right.resourceValue;
+        });
+
+    }
+
+    $scope.bridgeRight = newRight;
+//    console.log($scope.bridgeRight.indexOf('/deleteProtocol'));
+
 
 });
