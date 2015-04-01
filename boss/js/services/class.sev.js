74f6c89d3425d6ec1d629c5421e47d10b540c7d2
asfeixue <zxxiao@thinkjoy.cn>
2015-04-01 14:21:42 +0800

    修正班级详情接口错误

--- boss/js/services/class.sev.js
+++ boss/js/services/class.sev.js
@@ -9,38 +9,22 @@ App
 
             getClassMessageInfoByCode : function(classCode,userType,count,queryTime){
                 var defer =  $q.defer();
-//                $http.post(SERVER.url.message+"/messageBoss/getClassMessages",{
-//                    data : {
-//                        classId : classCode,
-//                        count  : count,
-//                        userType : userType,
-//                        queryTime : queryTime
-//                    }
-//                },{headers : {"is-json-data":1}})
-//                 .success(function(result){
-//                        defer.resolve(result);
-//                    })
-//                    .error(function(err){
-//                        console.error("class error");
-//                        defer.reject(err);
-//                  });
-
-                var  temp = $resource(SERVER.url.message+"/messageBoss/getClassMessages",{
-
-
-                });
-                temp.save({
-                    data : {
-                        classId: classCode,
-                        count: count,
-                        userType: userType,
-                        queryTime: queryTime
-                    }
-                  },function(res){
-                        console.log("diao...");
-                        console.log(res);
-                })
-
+               $http.post(SERVER.url.message+"/messageBoss/getClassMessages",
+                   {
+                       data:{
+                           classId : classCode,
+                           count  : count,
+                           userType : userType,
+                           queryTime : queryTime
+                       }
+                   },{headers : {"is-json-data":1}})
+                .success(function(result){
+                       defer.resolve(result);
+                   })
+                   .error(function(err){
+                       console.error("class error");
+                       defer.reject(err);
+                 });
                 return  defer.promise;
             },
 
